05f18949706b971778f31b0000870a0de7eb5479
drowsY <rexlee9228@gmail.com>
2025-03-23 06:44:36 -0400

    Add files via upload

---
+++ boj/2529.py
@@ -0,0 +1,43 @@
+k = int(input())
+num_list = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+inequality = input().split()
+check = [False] * 10
+answer = [0, 9999999999]
+def backtrack(i, result):
+    #print(result)
+    if i == k + 1:
+        if int(result) > int(answer[0]):
+            answer[0] = result
+        if int(result) < int(answer[1]):
+            answer[1] = result
+        #print(answer)
+        #print(k, i, result)
+        return
+    for j in num_list:
+        if not check[j]:
+            if (inequality[i - 1] == '<' and int(result[-1]) < j) or (inequality[i - 1] == '>' and int(result[-1]) > j):
+                #print(check, i, j)
+                check[j] = True
+                result += str(j)
+                i += 1
+                backtrack(i, result)
+                i -= 1
+                check[j] = False
+                #print(result)
+                result = result[:-1]
+                #print(result)
+                
+for i in range(9, -1, -1):
+    check[i] = True
+    backtrack(1, str(i))
+    check = [False] * 10
+#print(answer)
+print(answer[0])
+print(answer[1])
+'''
+2
+< >
+
+9
+> < < < > > > < <
+'''
